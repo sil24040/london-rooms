05619c8115c5bd05123a779789ee57f61f63fbbb
sbd488 <sil24040@byui.edu>
2026-07-15 23:18:33 -0600

    Revert "Fix Sign in Sign up nav buttons PT translation"
    This reverts commit 5d85c7a598c247e9094241af632896e36c6edca3.

--- client/i18n.js
+++ client/i18n.js
@@ -240,8 +240,6 @@ function applyTranslations() {
   safeSet('login-btn', t('signIn'));
   safeSet('register-btn', t('createAccount'));
   safeSet('profile-btn', t('saveChanges'));
-  safeSet('btn-login', t('signIn'));
-  safeSet('btn-register', t('signUp'));
 }
 
 // Initialise language on page load
